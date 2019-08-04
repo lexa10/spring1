0aacea33be9c103c86f4677ce4ea91f074cea923
adida_7sfihp9 <adidaslexa@mail.ru>
2019-08-04 16:48:12 +0300

    переделал

--- lesson2/src/main/java/spring/App.java
+++ lesson2/src/main/java/spring/App.java
@@ -1,16 +1,17 @@
 package spring;
 
 import spring.config.ApplicationConfig;
-import spring.service.BestService;
-import spring.service.MegaService;
+import spring.service.BetaService;
 
 public class App {
 
     public static  void main(String [] args){
         final ApplicationContext context = new AnnotationConfigApplicatonContext(ApplicationConfig.class);
-        final MegaService megaService = context.getBean(MegaService.class);
-        final BestService bestService = context.getBean(BestService.class);
-        System.out.println(megaService);
+
+        final BetaService bestService = context.getBean(BetaService.class);
+
+        System.out.println(bestService.getSimpleServiceBean().get);
+        System.out.println(bestService);
         System.out.println(bestService);
     }
 }
